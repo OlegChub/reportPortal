6cf9b7aa0ee718fc54a2a3af7dc90902423f44ba
Oleg_Chubryk <hoplla18@gmail.com>
2022-10-04 18:40:22 +0200

    add @Data tag

--- src/main/java/models/Page.java
+++ src/main/java/models/Page.java
@@ -1,5 +1,10 @@
 package models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Page {
     public int number;
     public int size;
