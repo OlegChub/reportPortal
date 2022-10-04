bd335c6b55d34281f8a7af64cd0c1831dd32be65
Oleg_Chubryk <hoplla18@gmail.com>
2022-10-04 18:10:39 +0200

    add allure

--- src/test/java/ApiTests.java
+++ src/test/java/ApiTests.java
@@ -1,4 +1,5 @@
-import models.Dashboard;
+import io.qameta.allure.Owner;
+import models.Root;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,21 +10,18 @@ import static specs.RestAssuredSpec.requestSpecification;
 import static specs.RestAssuredSpec.resSpec;
 
 public class ApiTests {
-
     @Test
     @DisplayName("Get all permitted dashboard resources for specified project")
+    @Owner("Oleg Chubryk")
     public void getAllPermittedDashboard() {
-
-        Dashboard response=
+        Root response =
                 given(requestSpecification)
-                .contentType("application/json; charset=UTF-8")
-                .when()
-                .get("api/v1/"+getProjectName()+"/dashboard")
-                .then()
-                .log().body()
-                .spec(resSpec)
-                .extract().as(Dashboard.class);
-        assertEquals(14,response.getId());
-
+                        .when()
+                        .get("api/v1/" + getProjectName() + "/dashboard")
+                        .then()
+                        .log().body()
+                        .spec(resSpec)
+                        .extract().as(Root.class);
+        assertEquals(14, response.content.get(0).getId());
     }
 }
