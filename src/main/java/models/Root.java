b0479873da04c7c304e1f267ca253ecca8a93b99
Oleg_Chubryk <hoplla18@gmail.com>
2022-10-05 11:45:52 +0200

    add taf

---
+++ src/main/java/models/Root.java
@@ -0,0 +1,14 @@
+package models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import java.util.ArrayList;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Root {
+        public ArrayList<Content> content;
+        public Page page;
+    }
+
---
+++ src/test/java/ApiTests.java
@@ -0,0 +1,27 @@
+import io.qameta.allure.Owner;
+import models.Root;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static helpers.ConfigSetUp.*;
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static specs.RestAssuredSpec.requestSpecification;
+import static specs.RestAssuredSpec.resSpec;
+
+public class ApiTests {
+    @Test
+    @DisplayName("Get all permitted dashboard resources for specified project")
+    @Owner("Oleg Chubryk")
+    public void getAllPermittedDashboard() {
+        Root response =
+                given(requestSpecification)
+                        .when()
+                        .get("api/v1/" + getProjectName() + "/dashboard")
+                        .then()
+                        .log().body()
+                        .spec(resSpec)
+                        .extract().as(Root.class);
+        assertEquals(14, response.content.get(0).getId());
+    }
+}
