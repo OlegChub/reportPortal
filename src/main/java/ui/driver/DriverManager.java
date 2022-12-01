2843019f90623a5fd5a04f85d7558f32ae622d8b
Oleg_Chubryk <hoplla18@gmail.com>
2022-12-01 12:18:54 +0100

    add driver config

--- src/main/java/ui/driver/DriverManager.java
+++ src/main/java/ui/driver/DriverManager.java
@@ -1,20 +1,16 @@
 package ui.driver;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import ui.driverConfig.DriverProperties;
 
 import java.time.Duration;
 import java.util.Optional;
 
-import static org.openqa.selenium.chrome.ChromeDriverService.CHROME_DRIVER_EXE_PROPERTY;
+import static ui.logger.UILogger.LOGGER;
 
 public class DriverManager {
-    private final static String CHROME_DRIVER_PATH = "src\\main\\resources\\driver\\chromedriver.exe";
     private final static int IMPLICIT_WAIT_TIMEOUT = 5;
     private final static int PAGE_LOAD_TIMEOUT = 60;
-    private final static Logger logger = LogManager.getLogger();
 
     private static ThreadLocal<WebDriver> ThreadLocal = new ThreadLocal<>();
 
@@ -22,14 +18,13 @@ public class DriverManager {
     }
 
     public static void setupDriver() {
-        logger.info("Setting driver...");
-        System.setProperty(CHROME_DRIVER_EXE_PROPERTY, CHROME_DRIVER_PATH);
-        WebDriver driver = new ChromeDriver();
+        LOGGER.info("Setting driver...");
+        WebDriver driver = DriverProperties.setUpDriverWithProperties();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICIT_WAIT_TIMEOUT));
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(PAGE_LOAD_TIMEOUT));
         ThreadLocal.set(driver);
-        logger.info("Driver was set successfully");
+        LOGGER.info("Driver was set successfully");
     }
 
     public static WebDriver getDriver() {
@@ -37,10 +32,10 @@ public class DriverManager {
     }
 
     public static void quitDriver() {
-        logger.info("Quitting driver...");
+        LOGGER.info("Quitting driver...");
         Optional.ofNullable(getDriver()).ifPresent(WebDriver ->
                 WebDriver.quit()
         );
-        logger.info("Driver was quited successfully");
+        LOGGER.info("Driver was quited successfully");
     }
 }
